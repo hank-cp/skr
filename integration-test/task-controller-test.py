c41dff64b3eaf25619b48cb6ba8b6854db36434f
zsx <18273117003@163.com>
2020-01-16 16:03:20 +0800

    #FU403 增加Skr以分布式方式启动后的单元测试 修改

--- integration-test/task-controller-test.py
+++ integration-test/task-controller-test.py
@@ -14,13 +14,12 @@ class TaskControllerTest(unittest.TestCase):
         self.assertTrue(response.status_code == requests.codes.ok)
         content_json_data = json.loads(response.content)
         # 数据不存在时，会返回False，确保数据存在，不能等于False
-        self.assertNotEqual(jsonpath(content_json_data, 'access-token'), False)
+        self.assertTrue(jsonpath(content_json_data, 'access-token'))
         access_token = jsonpath(content_json_data, 'access-token')[0]
         # token不能为空
         self.assertIsNotNone(access_token)
         # token不能为空字符串
         self.assertIsNot(access_token, "")
-        print(access_token)
         task_list_headers = {'Content-type': 'application/json', 'access-token': access_token}
         task_list_response = requests.get('http://127.0.0.1:8101/task/extensions', headers=task_list_headers)
         self.assertTrue(task_list_response.status_code == requests.codes.ok)
